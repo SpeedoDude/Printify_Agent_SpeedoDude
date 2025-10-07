ff550857a1505369569397bc15862217c2d07cac
tabisz.jeremy <tabisz.jeremy@gmail.com>
2025-10-07 10:32:12 +0000

    Changes before Firebase Studio auto-run

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -9,7 +9,7 @@
     # pkgs.go
     pkgs.python311
     pkgs.python311Packages.pip
-    pkgs.python311Packages.streamlit
+    pkgs.python311Packages.flask
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
