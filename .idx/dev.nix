da813a10b722963dd720e2be383fe67e8b637272
tabisz.jeremy <tabisz.jeremy@gmail.com>
2025-10-07 14:03:40 +0000

    a

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -4,10 +4,10 @@
   # Which nixpkgs channel to use.
   channel = "stable-23.11"; # or "unstable"
   # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.python311
-    pkgs.python311Packages.pip
-  ];
+  # packages = [
+  #   pkgs.python311
+  #   pkgs.python311Packages.pip
+  # ];
   # Sets environment variables in the workspace
   env = {};
   idx = {
